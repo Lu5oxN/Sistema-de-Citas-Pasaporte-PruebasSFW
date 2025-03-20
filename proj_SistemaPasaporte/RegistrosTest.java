7f1e0c93fa3f236d3ad83aba4fe1c53f29f7d362
Alessia <125239072+AlessiaSanAmez@users.noreply.github.com>
2025-03-20 14:26:50 -0600

    Prueba 5

--- proj_SistemaPasaporte/RegistrosTest.java
+++ proj_SistemaPasaporte/RegistrosTest.java
@@ -16,7 +16,7 @@ public class RegistrosTest {
                                  "2031-01-01", "Puebla", "Centro", fechaPasada, 
                                  "10:00 AM", "Renovación", 1);
             
-            System.out.println("Method executed without exception, past dates are being accepted.");
+            System.out.println("El programa acepta fechas que ya pasaron.");
         } catch (Exception e) {
             System.out.println("Exception thrown: " + e.getMessage());
             assertTrue(e.getMessage().contains("Fecha de cita inválida"));
@@ -35,22 +35,22 @@ public class RegistrosTest {
                                  "2031-01-01", "Puebla", "Centro", "2025-10-10", 
                                  "10:00 AM", "Renovación", 1);
 
-                System.out.println("User " + (i + 1) + " registered successfully.");
+                System.out.println("User " + (i + 1) + " registrado.");
                 System.out.flush();
 
                 arr[i].setCiudadSRE("Nueva Ciudad");
 
             } catch (Exception e){
-                fail("Failed to register user " + (i + 1));
+                fail("No se logró registrar el usuario " + (i + 1));
             }
         }
         for (int i = 0; i < 100; i++) {
-            // Access and verify the changes made to each Cita
             assertEquals("Nueva Ciudad", arr[i].getCiudadSRE(), "CiudadSRE should be 'Nueva Ciudad' for user " + (i + 1));
             // Optionally, print out the user's details to verify
             System.out.println("User " + (i + 1) + " - CiudadSRE: " + arr[i].getCiudadSRE());
             System.out.flush();
         }
+        System.out.println("User 100 - CiudadSRE: " + arr[100].getCiudadSRE());
     }
 
     // PL03 - Validar el CURP que sea el correcto
@@ -62,7 +62,7 @@ public class RegistrosTest {
                                  "2031-01-01", "Puebla", "Centro", "2025-03-28", 
                                  "10:00 AM", "Renovación", 1);
             
-            System.out.println("Method executed without exception, CURP is valid accepted.");
+            System.out.println("El programa acepta CURPs inválidos.");
         } catch (Exception e) {
             System.out.println("Exception thrown: " + e.getMessage());
             assertTrue(e.getMessage().contains("CURP inválido. Debe tener 10 dígitos"));
@@ -78,7 +78,7 @@ public class RegistrosTest {
                                  "2031-01-01", "Puebla", "Centro", "2025-03-28", 
                                  "18:00 PM", "Renovación", 1);
             
-            System.out.println("Method executed without exception, the time of the appointment is within the client attention hour range.");
+            System.out.println("El programa permite agendar citas en horarios no disponibles.");
         } catch (Exception e) {
             System.out.println("Exception thrown: " + e.getMessage());
             assertTrue(e.getMessage().contains("Error: La cita está agendada en un horario inválido. La atención al cliente es de 9:00 a 17:50"));
@@ -86,5 +86,22 @@ public class RegistrosTest {
     }
 
     // PL05 - No permite agendar mas de 1 cita activa
-
+    @Test
+    void citaActivas(){
+        try{
+            Cita cita1 = new Cita("Juan", "Perez", "Gomez", "2000-05-15", 
+                                 "PERJ000515HMCZPCA3", "P1234567", "2021-01-01", 
+                                 "2031-01-01", "Puebla", "Centro", "2025-10-10", 
+                                 "10:00 AM", "Renovación", 1);
+            Cita cita2 = new Cita("Juan", "Perez", "Gomez", "2000-05-15", 
+                                 "PERJ000515HMCZPCA3", "P1234567", "2021-01-01", 
+                                 "2031-01-01", "Puebla", "Centro", "2025-11-10", 
+                                 "10:00 AM", "Renovación", 1);
+                                 
+            System.out.println("El programa permite tener múltiples citas activas.");
+        } catch (Exception e) {
+            System.out.println("Exception thrown: " + e.getMessage());
+            assertTrue(e.getMessage().contains("Error: Ya tienes una cita activa. Borra o modifica tu cita "));
+        }
+    }
 }
